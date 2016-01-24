72698a0ed2173a1bdfee31141556241cd1f957d2
Bruno <br_u.no@hotmail.com>
2016-01-24 21:35:15 -0200

    Atualizado Layout TelaPrincipal

--- app/src/main/java/br/ufpi/es/checar/Visao/TelaPrincipal.java
+++ app/src/main/java/br/ufpi/es/checar/Visao/TelaPrincipal.java
@@ -1,8 +1,12 @@
 package br.ufpi.es.checar.Visao;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import br.ufpi.es.checar.R;
 
@@ -13,5 +17,49 @@ public class TelaPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
 
+        // TOOLBAR
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        //Disabilitar Back Icon ActionBar
+        assert ab != null;
+        ab.setDefaultDisplayHomeAsUpEnabled(false);
+        //Disabilitar nome App ActionBar
+        ab.setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+//
+//        MenuItem searchItem = menu.findItem(R.id.action_search);
+//        SearchView searchView =
+//                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                // User chose the "Settings" item, show the app settings UI...
+//                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
