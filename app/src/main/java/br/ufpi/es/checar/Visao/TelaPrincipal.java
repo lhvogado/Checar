d98d014c4b05bdebb845db4408823eb5c4255e71
Bruno <br_u.no@hotmail.com>
2016-01-24 18:05:10 -0200

    Corrigindo Erro

---
+++ app/src/main/java/br/ufpi/es/checar/Visao/TelaPrincipal.java
@@ -0,0 +1,17 @@
+package br.ufpi.es.checar.Visao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import br.ufpi.es.checar.R;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+
+    }
+}
