a1ddc15c98a58352761fc833358db17d1fc48505
lhvogado <lhvogado@gmail.com>
2016-02-17 23:48:01 -0300

    Adicionando a documentação nos métodos da camada de persistência.

--- app/src/main/java/br/ufpi/es/checar/Persistencia/ConexaoBD.java
+++ app/src/main/java/br/ufpi/es/checar/Persistencia/ConexaoBD.java
@@ -7,9 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class ConexaoBD extends SQLiteOpenHelper {
-
+    /**
+     * Variáveis que dão acesso ao banco de dados.
+     */
     static final String NOME_BANCO = "checar.db";
-    static final String TABELA = "cliente";
+    static final String TABELA_CLIENTES = "cliente";
+    static final String TABELA_CARROS = "carro";
     static final int VERSAO_BD = 1;
 
 
@@ -18,12 +21,15 @@ public class ConexaoBD extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase bd) {
-        String sql = "create table cliente(_id integer primary key autoincrement, CPF text not null, RG text not null, Nome text not null);";
-        bd.execSQL(sql);
+        //String sql = "create table cliente(_id integer primary key autoincrement, CPF text not null, RG text not null, Nome text not null);";
+        String carro = "create table carro(_id integer primary key autoincrement, Placa text not null, Fabricante text not null, Modelo text not null, Versao text not null,  Ano text not null, Cor text not null, Motor text not null);";
+        //bd.execSQL(sql);
+        bd.execSQL(carro);
     }
     @Override
     public void onUpgrade(SQLiteDatabase bd, int arg1, int arg2) {
-        bd.execSQL("drop table if exists" + TABELA);
+        //bd.execSQL("drop table if exists" + TABELA_CLIENTES);
+        bd.execSQL("drop table if exists" + TABELA_CARROS);
         onCreate(bd);
     }
 }
--- app/src/main/java/br/ufpi/es/checar/Persistencia/ControleBanco.java
+++ app/src/main/java/br/ufpi/es/checar/Persistencia/ControleBanco.java
@@ -11,9 +11,25 @@ public class ControleBanco {
     private SQLiteDatabase db;
     private ConexaoBD banco;
 
+    /**
+     * Cria uma nova conexão com o banco de dados, gerando uma nova tabela com os respectivos dados.
+     *
+     * @author Luis Henrique
+     * @param context
+     * @return ControleBanco
+     */
     public ControleBanco(Context context) {
         banco = new ConexaoBD(context);
     }
+
+    /**
+     * Insere no banco os dados CPF, RG e Nome associado ao novo Cliente.
+     * @author Luis Henrique
+     * @param cpf
+     * @param rg
+     * @param Nome
+     * @return - Retorna uma string com o valor de resultado para verificar se o dado foi inserido ou não.
+     */
     public String InserirDado(String cpf, String rg, String Nome) {
         ContentValues valores = new ContentValues();
         long resultado;
@@ -30,6 +46,45 @@ public class ControleBanco {
 
     }
 
+    /**
+     * Insere no banco dos dados dos veículos através da função getWritableDatabase(). É importante lembrar que
+     * o atributo db deve receber o resultado do método getWritableDatabase, que diz ao Android que o banco será
+     * utilizado para leitura e escrita de dados.
+     * @author Luis Henrique
+     * @param placa - Parâmetro
+     * @param fab - Fabricante
+     * @param ver - Versão
+     * @param mod - Modelo
+     * @param ano - Ano de fabricação
+     * @param cor - Cor do veículo
+     * @param motor - Motor do veículo
+     * @return - retorna uma string informando se o dado foi inserido ou não.
+     */
+    public String InserirPlaca(String placa, String fab, String ver, String mod, String ano, String cor, String motor) {
+        ContentValues valores = new ContentValues();
+        long resultado;
+        db = banco.getWritableDatabase();
+        valores.put("Placa", placa);
+        valores.put("Fabricante", fab);
+        valores.put("Modelo", mod);
+        valores.put("Versao", ver);
+        valores.put("Ano", ano);
+        valores.put("Cor", cor);
+        valores.put("Motor", motor);
+        resultado = db.insert("carro", null, valores);
+        db.close();
+        if(resultado == -1)
+            return "Erro ao inserir registro";
+        else
+            return "Registro Inserido com sucesso";
+
+    }
+
+    /**
+     * Lista todos os clientes cadastrados no banco de dados através da função getReadableDatabase().
+     * @author Luis Henrique
+     * @return - retorna um cursor uma classe do Android que salva as informações que são retornadas do banco de dados.
+     */
     public Cursor carregaDados(){
         Cursor c;
         String[] campos = {"_id","CPF","RG","Nome"};
@@ -41,6 +96,13 @@ public class ControleBanco {
         db.close();
         return c;
     }
+
+    /**
+     * Busca o cliente através do Cpf no banco de dados.
+     * @author Luis Henrique
+     * @param cpf
+     * @return - retorna um cursor uma classe do Android que salva as informações que são retornadas do banco de dados.
+     */
     public Cursor carregaDadoById(String cpf) {
         Cursor cursor;
         String[] campos = {"_id","CPF","RG","Nome"};
@@ -53,5 +115,41 @@ public class ControleBanco {
         db.close();
         return cursor;
     }
+
+    /**
+     * Busca uma placa a partir da seu ID no banco de dados.
+     * @param p - String placa, a placa do carro que vc deseja ver.
+     * @return - retorna um cursor uma classe do Android que salva as informações que são retornadas do banco de dados.
+     */
+    public Cursor BuscaPlaca(String p) {
+        Cursor cursor;
+        String[] campos = {"_id","Placa","Fabricante","Modelo","Versao","Ano","Cor","Motor"};
+        String where = "Placa" + "=" + p;
+        db = banco.getReadableDatabase();
+        cursor = db.query("carro", campos, where, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    /**
+     * Lista todos os veículos cadastrados no banco de dados.
+     * @author Luis Henrique
+     * @return - retorna um cursor uma classe do Android que salva as informações que são retornadas do banco de dados.
+     */
+    public Cursor ListarCarros() {
+        Cursor cursor;
+        String[] campos = {"_id","Placa","Fabricante","Modelo","Versao","Ano","Cor","Motor"};
+        db = banco.getReadableDatabase();
+        cursor = db.query("carro", campos, null, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
 }
+//
 
